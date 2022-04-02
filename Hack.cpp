18e58b46407235a08c24f7d7b4820dc09a33bc31
midamissio <102240743+midamissio@users.noreply.github.com>
2022-04-02 15:21:48 +0200

    Add files via upload

---
+++ Hack.cpp
@@ -0,0 +1,31 @@
+/*
+    Zadanie z platformy szkopul.edu.pl
+    Nazwa zadania: H4x0r
+    Link do zadania: https://szkopul.edu.pl/problemset/problem/-sW59iNCsLGhh8xyFPEmtn7F/site/?key=statement
+    
+*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    string napis_hackerski;
+
+    cin >> napis_hackerski;
+
+    for (int i = 0; i < napis_hackerski.size(); i++)
+    {
+        if (napis_hackerski[i] == 'a')
+            napis_hackerski[i] = '4';
+        else if (napis_hackerski[i] == 'e')
+            napis_hackerski[i] = '3';
+        else if (napis_hackerski[i] == 'i')
+            napis_hackerski[i] = '1';
+        else if (napis_hackerski[i] == 'o')
+            napis_hackerski[i] = '0';
+        else if (napis_hackerski[i] == 's')
+            napis_hackerski[i] = '5';
+    }
+
+    cout << napis_hackerski;
+}
